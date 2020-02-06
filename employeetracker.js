7783f7fb7b48f4e3ff6d8ee2d7d439b081908cf8
unknown <53538316+grlucy@users.noreply.github.com>
2020-02-06 15:16:47 -0500

    added viewData function to employeetracker.js

--- employeetracker.js
+++ employeetracker.js
@@ -62,9 +62,92 @@ function startApp() {
     });
 }
 
-function viewData() {}
+function viewData() {
+  inquirer
+    .prompt({
+      type: "list",
+      name: "viewItem",
+      message: "What would you like to view?",
+      choices: [
+        "All departments",
+        "All roles",
+        "All employees",
+        "Employees by manager",
+        "Employees by department",
+        "Total utilized budget of a specific department"
+      ]
+    })
+    .then(answer => {
+      switch (answer.viewItem) {
+        case "All departments":
+          connection.query(`SELECT * FROM department`, function(err, res) {
+            if (err) throw err;
+            console.table(res);
+            startApp();
+          });
+          break;
+        case "All roles":
+          connection.query(`SELECT * FROM role`, function(err, res) {
+            if (err) throw err;
+            console.table(res);
+            startApp();
+          });
+          break;
+        case "All employees":
+          connection.query(`SELECT * FROM employee`, function(err, res) {
+            if (err) throw err;
+            console.table(res);
+            startApp();
+          });
+          break;
+        case "Employees by manager":
+          // Come back to this
+          startApp();
+          break;
+        case "Employees by department":
+          connection.query(`SELECT * FROM department`, function(err, res) {
+            if (err) throw err;
+            inquirer
+              .prompt([
+                {
+                  name: "deptChoice",
+                  type: "rawlist",
+                  message: "Which department?",
+                  choices: function() {
+                    let choiceArray = [];
+                    for (let i = 0; i < res.length; i++) {
+                      choiceArray.push(res[i].name);
+                    }
+                    return choiceArray;
+                  }
+                }
+              ])
+              .then(answer => {
+                connection.query(
+                  `SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, employee.manager_id, department.name AS department_name FROM employee INNER JOIN role ON employee.role_id = role.id INNER JOIN department ON role.department_id = department.id WHERE department.name = "${answer.deptChoice}"`,
+                  function(err2, res2) {
+                    if (err2) throw err2;
+                    console.table(res2);
+                    startApp();
+                  }
+                );
+              });
+          });
+          break;
+        case "Total utilized budget of a specific department":
+          // come back to this
+          break;
+        default:
+          console.log("Error, please try again");
+          startApp();
+      }
+    });
+}
+
 function addData() {}
+
 function updateData() {}
+
 function deleteData() {}
 
 function exitApp() {
