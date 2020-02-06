8deb710e2e69d218a3e31dbfc68fcae78e5eb62e
unknown <53538316+grlucy@users.noreply.github.com>
2020-02-06 13:50:41 -0500

    added startApp function to employeetracker.js

--- employeetracker.js
+++ employeetracker.js
@@ -1,8 +1,14 @@
+// Enter your mysql password:
+
+const myPassword = "";
+
+// -------------------------------------------------------------------
 // Dependencies
 
 const inquirer = require("inquirer");
 const mysql = require("mysql");
 const consoleTable = require("console.table");
+const colors = require("colors");
 
 // -------------------------------------------------------------------
 // Connection
@@ -11,14 +17,63 @@ const connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "",
+  password: myPassword,
   database: "employeeTracker_DB"
 });
 
 connection.connect(err => {
   if (err) throw err;
+  console.log(
+    "----------- ".white.bgWhite.bold +
+      "WELCOME TO EMPLOYEE TRACKER!".blue.bgWhite.bold +
+      " ----------".white.bgWhite.bold
+  );
   startApp();
 });
 
 // -------------------------------------------------------------------
 // Functions
+
+function startApp() {
+  inquirer
+    .prompt({
+      type: "list",
+      name: "action",
+      message: "What would you like to do?",
+      choices: ["View", "Add", "Update", "Delete", "Exit"]
+    })
+    .then(answer => {
+      switch (answer.action) {
+        case "View":
+          viewData();
+          break;
+        case "Add":
+          addData();
+          break;
+        case "Update":
+          updateData();
+          break;
+        case "Delete":
+          deleteData();
+          break;
+        default:
+          exitApp();
+      }
+    });
+}
+
+function viewData() {}
+function addData() {}
+function updateData() {}
+function deleteData() {}
+
+function exitApp() {
+  console.log(
+    "------ ".white.bgWhite.bold +
+      "THANK YOU FOR USING EMPLOYEE TRACKER!".blue.bgWhite.bold +
+      " ------\n--------------------- ".white.bgWhite.bold +
+      "GOODBYE".blue.bgWhite.bold +
+      " ---------------------".white.bgWhite.bold
+  );
+  connection.end();
+}
